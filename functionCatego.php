ad2269ad809db9e1ac420e451fa832b4419aab19
VictorCouturieux <victor.couturieux@laposte.net>
2015-11-23 14:28:42 +0100

    correction clés étrangères

--- functionCatego.php
+++ functionCatego.php
@@ -5,7 +5,7 @@
 		//variable associer au tableaux d'information sur l' article 
 		$article = queryDB("SELECT id_Categorie 
                              FROM Categorie
-                            WHERE cle_etrangere = " . $id_Article); 
+                            WHERE Categorie_article = " . $id_Article); 
     	return $article;
   	}
 
@@ -16,11 +16,13 @@
 		//variable associer au tableaux d'information sur la categorie
     	$string = "UPDATE id_Categorie, intitule_Categorie, description_Categorie,  
 		 			FROM Categorie
+		 			WHERE Categorie_article
                 "; 
     	//variable associer au tableaux d'information sur la categorie
     	$article = queryDB($string);
-    
-    	return !($article == $string);
+    	
+    	//verification booleen de validation (inutile)
+    	//return !($article == $string);
   	}
 
 ?>
