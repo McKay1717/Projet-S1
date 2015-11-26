edb3b59556e00274cd89e117b9c80a3eb4314bca
Nicolas <nicolas@mckay1717.eu>
2015-11-26 18:44:33 +0100

    Mise à jour du MRD

--- projet-s1.sql
+++ projet-s1.sql
@@ -3,19 +3,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données: `projet_tut_S1`
---
-CREATE DATABASE IF NOT EXISTS `projet_tut_S1` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `projet_tut_S1`;
-
--- --------------------------------------------------------
-
 --
 -- Structure de la table `Article`
 --
@@ -23,7 +10,7 @@ USE `projet_tut_S1`;
 CREATE TABLE IF NOT EXISTS `Article` (
   `id_Article` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `contenu_Article` text NOT NULL,
-  `date_parution_Article` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_parution_Article` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `nom_Article` varchar(255) NOT NULL,
   `Categorie_Article` int(10) unsigned NOT NULL,
   `last_modification_Article` int(10) unsigned NOT NULL,
@@ -39,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `Article` (
 --
 
 INSERT INTO `Article` (`id_Article`, `contenu_Article`, `date_parution_Article`, `nom_Article`, `Categorie_Article`, `last_modification_Article`, `Auteur_Article`) VALUES
-(1, 'Ceci est un test', '2015-11-23 19:47:42', 'Ceci est un nom', 1, 1, 1);
+(1, 'Ceci est un test', '2015-11-23 18:47:42', 'Ceci est un nom', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -69,7 +56,7 @@ INSERT INTO `Categorie` (`id_Categorie`, `intitule_Categorie`, `description_Cate
 
 CREATE TABLE IF NOT EXISTS `Connexion` (
   `id_Connexion` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `Date_Connexion` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Date_Connexion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `User_Connexion` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id_Connexion`,`User_Connexion`),
   KEY `link-to-user-connextion` (`User_Connexion`)
@@ -80,9 +67,9 @@ CREATE TABLE IF NOT EXISTS `Connexion` (
 --
 
 INSERT INTO `Connexion` (`id_Connexion`, `Date_Connexion`, `User_Connexion`) VALUES
-(1, '2015-11-21 17:44:03', 1),
-(2, '2015-11-21 17:47:36', 1),
-(3, '2015-11-21 18:00:28', 1);
+(1, '2015-11-21 16:44:03', 1),
+(2, '2015-11-21 16:47:36', 1),
+(3, '2015-11-21 17:00:28', 1);
 
 -- --------------------------------------------------------
 
@@ -92,7 +79,7 @@ INSERT INTO `Connexion` (`id_Connexion`, `Date_Connexion`, `User_Connexion`) VAL
 
 CREATE TABLE IF NOT EXISTS `Modification` (
   `id_Modification` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `Date_Modification` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Date_Modification` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_Modification`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
@@ -101,7 +88,7 @@ CREATE TABLE IF NOT EXISTS `Modification` (
 --
 
 INSERT INTO `Modification` (`id_Modification`, `Date_Modification`) VALUES
-(1, '2015-11-23 19:46:54');
+(1, '2015-11-23 18:46:54');
 
 -- --------------------------------------------------------
 
@@ -111,7 +98,7 @@ INSERT INTO `Modification` (`id_Modification`, `Date_Modification`) VALUES
 
 CREATE TABLE IF NOT EXISTS `Utilisateur` (
   `id_Utilisateur` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `Date_Utilisateur` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Date_Utilisateur` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `MDP_Utilisateur` varchar(132) NOT NULL,
   `Mail_Utilisateur` varchar(255) NOT NULL,
   `nom_Utilisateur` varchar(50) NOT NULL,
@@ -123,7 +110,7 @@ CREATE TABLE IF NOT EXISTS `Utilisateur` (
 --
 
 INSERT INTO `Utilisateur` (`id_Utilisateur`, `Date_Utilisateur`, `MDP_Utilisateur`, `Mail_Utilisateur`, `nom_Utilisateur`) VALUES
-(1, '2015-11-21 17:22:08', '65829e542dd151f443cc997270c61e78c042a82d687cc13844bf2c1813714600', 'test@mail.tld', '');
+(1, '2015-11-21 16:22:08', '65829e542dd151f443cc997270c61e78c042a82d687cc13844bf2c1813714600', 'test@mail.tld', '');
 
 --
 -- Contraintes pour les tables exportées
