35455981cfab515e6fd034a758c3594437b3ce38
VictorCouturieux <victor.couturieux@laposte.net>
2015-11-26 10:50:50 +0100

    Update functionCatego.php

--- functionCatego.php
+++ functionCatego.php
@@ -1,7 +1,9 @@
 <?php
+
+
   	//fonction get: recuperer la categorie d'un article
   	function categoGet($id_Article)
-	{
+  	{
 		//variable associer au tableaux d'information sur l' article 
 		$article = queryDB("SELECT id_Categorie 
                              FROM intitule_Categorie
@@ -10,28 +12,28 @@
   	}
 
 
-  	//foncton set: definit la categolol d'un article
+
+  	//fonction set: definit la categorie d'un article
   	function categoSet($id_Article,$nom_CategoString)
   	{
 		$selectQuery = "SELECT id_Categorie 
 				FROM Categorie 
 				WHERE intitule_Categorie = " . $nom_CategoString;
 				
-		//tableau d'information sur le nom de la categolol	 	
+		//tableau d'information sur le nom de la categorie	 	
 		$catego = queryDB($selectQuery)['id_Categorie'];
 		
-		//variable associer a la mise a jour de la categolol
+		//variable associer a la mise a jour de la categorie
     		$updateQuery = "UPDATE Article
 		 	SET Categorie_article = " . $catego .
 		 	"WHERE id_Article = " . $id_Article;
 		 	
-    		//tableaux d'information sur la categolol
+    		//tableaux d'information sur la categorie
     		queryDB($updateQuery);
-   
-    		//verification booleen de validation (inutile)
-    		//return !($article == $string);
   	}
-  	
+
+
+
   	//fonction retournant la liste de tout les IDs des categories
   	function listIDcatego()
   	{
@@ -40,7 +42,9 @@
   			 ");
   		return $categorie;
   	}
-  	
+
+
+
   	//fonction retournant la liste de tout les noms des categories
   	function listNOMcatego()
   	{
@@ -49,5 +53,18 @@
   			 ");
   		return $categorie;
   	}
+
+
+
+    //fonction retournant la liste de tout les Articles dans une categorie
+    function listArticlINcatego($id_Categorie)
+    {
+      $listAtricl= queryDB("SELECT id_Article
+        FROM Article
+        WHERE Categorie_article = " . $id_Categorie
+          );
+      return $listAtricl;
+    }
+
 
 ?>
