02a071c7b6022ba40ce0d30a58e6c292a516c4d1
Nicolas <nicolas1706ni@gmail.com>
2015-11-26 19:00:48 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@ $config = array
 		(
 			"mysql" => array
 			(
-				"host" => "localhost",
-				"password" => "bahamut",
-				"user" => "root",
-				"db" => "projet_tut_S1",
+				"host" => "",
+				"password" => "",
+				"user" => "",
+				"db" => "",
 				"port" => 3306
 			)
 		);
