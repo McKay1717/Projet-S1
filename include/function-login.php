01f1833eb22e378db608840773eb09801127fb87
Raphael-R-R <raphael.ragoomundun.act@openmailbox.org>
2015-11-29 12:30:55 +0100

    Destrustion de session
    Destruction des données contenues dans la super globale $_SESSION et suppression des cookies. Cf http://php.net/manual/fr/function.session-destroy.php

--- include/function-login.php
+++ include/function-login.php
@@ -56,7 +56,18 @@
     // Met fin à la session en cours pour déconnecter l'utilisateur
     function disconnect()
     {
-        return session_destroy ();
+        $_SESSION = array();
+        
+        if (ini_get("session.use_cookies"))
+        {
+            $params = session_get_cookie_params();
+            
+            setcookie(session_name(), '', time() - 42000, 
+            $params["path"], $params["domain"], $params["secure"], 
+            $params["httponly"]);
+        }
+        
+        session_destroy ();
     }
 
 
