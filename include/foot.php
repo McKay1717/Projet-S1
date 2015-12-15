310969c6a76b66313a32160221b81b897431a086
Helladan <anthony.godard@edu.univ-fcomte.fr>
2015-12-15 13:38:10 +0100

    Ajout des mentions légales

--- include/foot.php
+++ include/foot.php
@@ -1,5 +1,10 @@
 <footer>
 	<p>&copy; <?php echo date("Y");?>
 		Tous droits réservés.
-		Créé par Victor Couturieux, Anthony Godard, Nicolas Iung, Florent Martin et Raphael Ragoomundun</p>
+		Créé par Victor Couturieux, Anthony Godard, Nicolas Iung, Florent Martin et Raphael Ragoomundun
+        <br>
+        <a href="legals.php">
+            Mentions légales
+        </a>
+    </p>
 </footer>
